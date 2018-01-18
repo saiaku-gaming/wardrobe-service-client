910af5b756c8ba4f412cdd837b9db7cd811c88e3
Mikael Berglund <mikbe@kth.se>
2018-01-18 23:29:44 +0100

    Fixes stupid...

--- src/main/java/com/valhallagame/wardrobeserviceclient/message/WardrobeItem.java
+++ src/main/java/com/valhallagame/wardrobeserviceclient/message/WardrobeItem.java
@@ -1,8 +1,42 @@
 package com.valhallagame.wardrobeserviceclient.message;
 
 public enum WardrobeItem {
+	// ------------------------ ARMORS -------------------
+	CLOTH_ARMOR,
 	LEATHER_ARMOR,
+	MAIL_ARMOR,
+	SHAMANS_PELT,
+	RANGERS_SAFEGUARD,
+	CHIEFTAINS_CUIRASS,
+	// ---------------------------------------------------
+
+	// --------------------- UNIQUE ARMORS ---------------
+	FROST_SHAMAN,
+	// ---------------------------------------------------
+	
+	// --------------------- WEAPONS ---------------------
 	SWORD,
+	LONGSWORD,
+	HAND_AXE,
+	DAGGER,
+	GREATAXE,
+	WARHAMMER,
+	BOW,
+	// ---------------------------------------------------
+
+	// --------------------- OFFHAND ---------------------
+	TORCH,
+	// ---------------------------------------------------
+
+	// --------------------- SHIELDS ---------------------
+	SMALL_SHIELD,
 	MEDIUM_SHIELD,
-	MAIL_ARMOR
+	LARGE_SHIELD,
+	STEEL_SHIELD,
+	// ---------------------------------------------------
+
+	// ------------------ UNIQUE WEAPONS -----------------
+	FROST_SHAMANS_GREATAXE,
+	FIRE_SHAMANS_GREATSWORD
+	// ---------------------------------------------------
 }
