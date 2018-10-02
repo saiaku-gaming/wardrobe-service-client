58db91b2fcdc56b57e0042ab13a1bf54ad8b0e63
Mikael Berglund <mikbe@kth.se>
2018-10-02 23:45:18 +0200

    Small cleanup on clients

--- src/main/java/com/valhallagame/wardrobeserviceclient/WardrobeServiceClient.java
+++ src/main/java/com/valhallagame/wardrobeserviceclient/WardrobeServiceClient.java
@@ -1,45 +1,41 @@
 package com.valhallagame.wardrobeserviceclient;
 
-import java.io.IOException;
-import java.util.List;
-
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.valhallagame.common.AbstractServiceClient;
 import com.valhallagame.common.DefaultServicePortMappings;
-import com.valhallagame.common.RestCaller;
 import com.valhallagame.common.RestResponse;
 import com.valhallagame.wardrobeserviceclient.message.AddWardrobeItemParameter;
 import com.valhallagame.wardrobeserviceclient.message.GetWardrobeItemsParameter;
 
-public class WardrobeServiceClient {
+import java.io.IOException;
+import java.util.List;
+
+public class WardrobeServiceClient extends AbstractServiceClient {
 	private static WardrobeServiceClient wardrobeServiceClient;
 
-	private String wardrobeServiceServerUrl = "http://localhost:" + DefaultServicePortMappings.WARDROBE_SERVICE_PORT;
-	private RestCaller restCaller;
-
 	private WardrobeServiceClient() {
-		restCaller = new RestCaller();
+		serviceServerUrl = "http://localhost:" + DefaultServicePortMappings.WARDROBE_SERVICE_PORT;
 	}
 
-	public static void init(String wardrobeServiceServerUrl) {
+	public static void init(String serviceServerUrl) {
 		WardrobeServiceClient client = get();
-		client.wardrobeServiceServerUrl = wardrobeServiceServerUrl;
+		client.serviceServerUrl = serviceServerUrl;
 	}
 
 	public static WardrobeServiceClient get() {
 		if (wardrobeServiceClient == null) {
 			wardrobeServiceClient = new WardrobeServiceClient();
 		}
-
 		return wardrobeServiceClient;
 	}
 
 	public RestResponse<List<String>> getWardrobeItems(String username) throws IOException {
-		return restCaller.postCall(wardrobeServiceServerUrl + "/v1/wardrobe/get-wardrobe-items",
+		return restCaller.postCall(serviceServerUrl + "/v1/wardrobe/get-wardrobe-items",
 				new GetWardrobeItemsParameter(username), new TypeReference<List<String>>() {});
 	}
 
 	public RestResponse<String> addWardrobeItem(AddWardrobeItemParameter input) throws IOException {
-		return restCaller.postCall(wardrobeServiceServerUrl + "/v1/wardrobe/add-wardrobe-item",
+		return restCaller.postCall(serviceServerUrl + "/v1/wardrobe/add-wardrobe-item",
 				input, String.class);
 	}
 }
