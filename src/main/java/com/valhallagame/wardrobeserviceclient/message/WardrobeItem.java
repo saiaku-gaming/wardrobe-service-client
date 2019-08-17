1aee8acb73696ce43f2cfefaa6b2fb49532419d3
phrozen <phrozen@teamsamst.com>
2019-08-17 19:17:42 +0200

    Updated wardrobe items to correspond to ArmamentItem and ArmorItem

--- src/main/java/com/valhallagame/wardrobeserviceclient/message/WardrobeItem.java
+++ src/main/java/com/valhallagame/wardrobeserviceclient/message/WardrobeItem.java
@@ -2,20 +2,87 @@ package com.valhallagame.wardrobeserviceclient.message;
 
 public enum WardrobeItem {
 	// ------------------------ ARMORS -----------------------
+    ODINS_ROBES, //Dev armor for when one wants to run around like a god
+
 	NAKED,
 	CLOTH_ARMOR,
 	LEATHER_ARMOR,
 	MAIL_ARMOR,
-	SHAMANS_PELT,
-	RANGERS_SAFEGUARD,
-	CHIEFTAINS_CUIRASS,
-	// -------------------------------------------------------
 
-	// --------------------- UNIQUE ARMORS -------------------
+    WORN_CLOTH_ARMOR,
+    DAMAGED_LEATHER_ARMOR,
+    RUSTY_MAIL_ARMOR,
+
+	SHAMANS_PELT,
+    RANGERS_SAFEGUARD_OLD,
+	CHIEFTAINS_CUIRASS,
+
+
+    // -------------------------------------------------------
+
+
+    // --------------------- UNIQUE ARMORS -------------------
 	FROST_SHAMAN,
 	// -------------------------------------------------------
-	
+
+    // ------------------------ HEAD -----------------------
+
+    // ------------------------ BEARD -----------------------
+
+    // ------------------------ CHEST -----------------------
+    CLOTH_TUNIC,
+    RANGERS_HARNESS,
+    HEAVY_HIDE_CHESTPIECE,
+
+    FINE_WOVEN_TUNIC,
+    LEATHER_SAFEGUARD,
+    LAYERED_HIDE_CHESTPIECE,
+
+    WADMAL_TUNIC,
+    RANGERS_SAFEGUARD,
+    ADVENTURERS_HIDE,
+
+    CHIEFTAINS_PELT,
+    QUARTERMASTERS_CUIRASS,
+    BELT_OF_POWER,
+
+    // ------------------------ GLOVES -----------------------
+
+    LUMBERJACKS_GLOVES,
+    RANDOM_GLOVES,
+
+    // ------------------------ PANTS -----------------------
+    WORN_RAGS,
+
+    CLOTH_PANTS,
+    LEATHER_PANTS,
+    REINFORCED_LEATHER_PANTS,
+
+    WADMAL_PANTS,
+    LEATHER_BREECHES,
+    REINFORCED_LEATHER_BREECHES,
+    ALCHEMISTS_KILT,
+
+    // ------------------------ BOOTS -----------------------
+    SIMPLE_BOOTS,
+    LEATHER_FOOTWEAR,
+    REINFORCED_BOOTS,
+
+    APPRENTICES_BOOTS,
+    LEATHER_BOOTS,
+    FORTIFIED_BOOTS,
+
 	// --------------------- WEAPONS ---------------------
+    THE_MURDERIZER, //Dev weapon to more easily murderize one's way through the levels
+
+    RUSTY_SWORD,
+    UNBALANCED_LONGSWORD,
+    BLUNT_HAND_AXE,
+    RUSTY_DAGGER,
+    BLUNT_GREATAXE,
+    UNBALANCED_WARHAMMER,
+    WEAK_HUNTING_BOW,
+
 	SWORD,
 	LONGSWORD,
 	HAND_AXE,
@@ -30,6 +97,11 @@ public enum WardrobeItem {
 	// ---------------------------------------------------
 
 	// --------------------- SHIELDS ---------------------
+    CUMBERSOME_SMALL_SHIELD,
+    OLD_MEDIUM_SHIELD,
+    TARNISHED_LARGE_SHIELD,
+    RUSTY_STEEL_SHIELD,
+
 	SMALL_SHIELD,
 	MEDIUM_SHIELD,
 	LARGE_SHIELD,
@@ -37,7 +109,7 @@ public enum WardrobeItem {
 	// ---------------------------------------------------
 
 	// ------------------ UNIQUE WEAPONS -----------------
-	FROST_SHAMANS_GREATAXE,
+    GRYNMAS_CLUB,
 	FIRE_SHAMANS_GREATSWORD
 	// ---------------------------------------------------
 }
